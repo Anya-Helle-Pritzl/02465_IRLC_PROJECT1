dc476a62b23275275513bfbe1c5f4a12349be390
OliverJessen <oliver.negendahl@gmail.com>
2026-05-08 10:40:31 +0200

    whoops

--- irlc/project4/rebels.py
+++ irlc/project4/rebels.py
@@ -12,7 +12,7 @@ very_basic_grid = [['#',1, '#'],
 
 
 # TODO: 21 lines missing.
-raise NotImplementedError("I wrote an agent that inherited from the Q-agent, and updated the self.pi and self.train-functions to do UCB-based exploration.")
+# raise NotImplementedError("I wrote an agent that inherited from the Q-agent, and updated the self.pi and self.train-functions to do UCB-based exploration.")
 
 def get_ucb_actions(layout : list, alpha : float, c : float, episodes : int, plot=False) -> list: 
     """ Return the sequence of actions the agent tries in the environment with the given layout-string when trained over 'episodes' episodes.
